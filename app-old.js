a84c973778c3cc159a8fc09170043e0d151e093b
Matias Luarte <matias.luarte@hotmail.com>
2022-07-13 19:07:26 -0400

    intro a express

---
+++ app-old.js
@@ -0,0 +1,31 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+
+    // res.writeHead(200, { 'Content-Type': 'application/json'});
+    // res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+    // res.writeHead(200, { 'Content-Type': 'application/csv'});
+
+
+    // const persona = {
+    //     id:1,
+    //     nombre: 'Matias'
+    // }
+
+    
+    res.write( 'Hola Mundo' );
+    res.end();
+
+
+})
+.listen( 8080 );
+
+console.log('Escuchando el puerto ',8080);
+
+
+
+
+
+
+
+   
--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-   
+const express = require('express')
+const app = express()
 
+//navegacion entre paginas
+app.get('/',  (req, res) => {
+  res.send('Home page')
+});
 
+app.get('/hola-mundo',  (req, res) => {
+    res.send('Hola mundo desde su respectiva ruta')
+});
 
+//peticion get que muestra mensage de not found '*'
+app.get('*',  (req, res)  => {
+    res.send('404 | page not found')
+});
 
-
-
-   
+app.listen(8080)
