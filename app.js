846aeb6b60bd8398a3c9efb6c7f655c56deb88c2
Matias Luarte <matias.luarte@hotmail.com>
2022-07-13 19:47:36 -0400

    sirviendo sitio web

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const express = require('express')
 const app = express()
+const port = 8080;
+
+//middleware
+// Servir contenido estatico
+app.use( express.static('public') );
 
 //navegacion entre paginas
-app.get('/',  (req, res) => {
-  res.send('Home page')
+// app.get('/',  (req, res) => {
+//   res.send('Home page');
+// });
+
+app.get('/generic',  (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html');
 });
 
-app.get('/hola-mundo',  (req, res) => {
-    res.send('Hola mundo desde su respectiva ruta')
+app.get('/elements',  (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html');
 });
 
 //peticion get que muestra mensage de not found '*'
 app.get('*',  (req, res)  => {
-    res.send('404 | page not found')
+    res.sendFile(__dirname +'/public/404.html');
 });
 
-app.listen(8080)
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
